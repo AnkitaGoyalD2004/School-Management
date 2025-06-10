1f9bcac8796fadce2b3d7f51517e866cd5cf9c01
Ankita Goyal <goyalankita0325@gmail.com>
2025-06-10 21:41:30 +0530

    working on CHART

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,5 +1,26 @@
+import UserCard from "@/components/UserCard";
+
 const AdminPage = () => {
-  return <div className="">AdminPage</div>;
+  return <div className="p-4 flex gap-4 flex-col md:flex-row">
+     {/* Left */}
+     <div className="w-full lg:w-2/3">
+            {/* USER CARDS */}
+            <div className="flex gap-4 justify-between flex-wrap">
+                <UserCard type="student" />
+                <UserCard type="teacher" />
+                <UserCard type="parent" />
+                <UserCard type="staff" />
+            </div>
+            {/* MIDDLE CHARTS */}
+            <div className="flex gap-4 flex-col lg:flex-row">
+              Count Chart 
+            </div>
+            {/* BOTTOM CHARTS */}
+            <div className=""></div>
+     </div>
+     {/* Right */}
+     <div className="w-full lg:w-1/3">r</div>
+  </div>;
 };
 
 export default AdminPage;
