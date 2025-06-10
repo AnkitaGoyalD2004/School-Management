b0e6d2f2d1870c1a2088ee7d4d2b73bc8099ead2
Ankita Goyal <goyalankita0325@gmail.com>
2025-06-10 16:59:07 +0530

    update

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -12,7 +12,7 @@ export default function DashboardLayout({
         <div className="h-screen flex">
           {/* Left */}
           <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%]">
-            <Link href="/" 
+            <Link href="/"
                className="flex items-center justify-center lg:justify-start gap-2 p-4">
                  <Image src="/logo.png" alt="logo" width={32} height={32} />
                  <span className="hidden lg:block">School Management</span>
