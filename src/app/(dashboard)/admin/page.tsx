f061c85d184be512b3732ac2d3ce160b1c97bb10
Ankita Goyal <goyalankita0325@gmail.com>
2025-06-12 16:28:29 +0530

    done with charts

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,25 +1,41 @@
+import AttendanceChart from "@/components/AttendanceChart";
+import CountChart from "@/components/CountChart";
+import FinanceChart from "@/components/FinanceChart";
 import UserCard from "@/components/UserCard";
 
 const AdminPage = () => {
   return <div className="p-4 flex gap-4 flex-col md:flex-row">
-     {/* Left */}
-     <div className="w-full lg:w-2/3">
-            {/* USER CARDS */}
-            <div className="flex gap-4 justify-between flex-wrap">
-                <UserCard type="student" />
-                <UserCard type="teacher" />
-                <UserCard type="parent" />
-                <UserCard type="staff" />
-            </div>
-            {/* MIDDLE CHARTS */}
-            <div className="flex gap-4 flex-col lg:flex-row">
-              Count Chart 
-            </div>
-            {/* BOTTOM CHARTS */}
-            <div className=""></div>
-     </div>
-     {/* Right */}
-     <div className="w-full lg:w-1/3">r</div>
+    {/* Left */}
+    <div className="w-full lg:w-2/3 flex flex-col gap-8">
+      {/* USER CARDS */}
+      <div className="flex gap-4 justify-between flex-wrap">
+        <UserCard type="student" />
+        <UserCard type="teacher" />
+        <UserCard type="parent" />
+        <UserCard type="staff" />
+      </div>
+      {/* MIDDLE CHARTS */}
+      <div className="flex gap-4 flex-col lg:flex-row">
+
+        {/* Count Chart  */}
+        <div className="w-full lg:w-1/3 h-[450px]">
+             <CountChart/>
+        </div>
+
+        {/* Attendance chart */}
+        <div className="w-full lg:w-2/3 h-[450px]">
+             <AttendanceChart/>
+        </div>
+
+      </div>
+      {/* BOTTOM CHARTS */}
+      <div className=" w-full h-[500px]">
+        <FinanceChart/>
+      </div>
+
+    </div>
+    {/* Right */}
+    <div className="w-full lg:w-1/3">r</div>
   </div>;
 };
 
