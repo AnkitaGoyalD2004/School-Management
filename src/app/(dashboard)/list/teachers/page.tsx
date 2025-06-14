8b6c3f4f6111b567ca0be87ae1070cf51a79928f
Ankita Goyal <goyalankita0325@gmail.com>
2025-06-14 21:08:29 +0530

    update

---
+++ src/app/(dashboard)/list/teachers/page.tsx
@@ -0,0 +1,13 @@
+const TeacherListPage = () => {
+    return (
+        <div className="bg-white p-4 rounded-md flex-1 m-4 mt-0">
+        {/* TOP */}
+        <div className=''></div>
+        {/* LIST */}
+        <div className=''></div>
+        {/* PAGINATION */}
+        <div className=''></div>
+        </div>
+    )
+}
+export default TeacherListPage;
