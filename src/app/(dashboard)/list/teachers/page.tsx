214043a0b0f811044e0915103f07fca5c42bbc72
Ankita Goyal <goyalankita0325@gmail.com>
2025-07-07 16:42:17 +0530

    update

--- src/app/(dashboard)/list/teachers/page.tsx
+++ src/app/(dashboard)/list/teachers/page.tsx
@@ -109,18 +109,28 @@ const TeacherListPage = async ({
 
   const {page , ...queryParams} = searchParams;
 
-  const p = page  ? parseInt(page) :  1;
+  const p = page  ? parseInt(page) :  1 ;
   
   const [data , count] = await prisma.$transaction([
     prisma.teacher.findMany({
-      include: {
+
+where:{
+ lessons:{
+  some:{classId:parseInt(queryParams.classId!)}
+ }
+},
+    include: {
         subjects: true,
         classes: true,
       },
       take: ITEM_PER_PAGE,
       skip: (p - 1) * ITEM_PER_PAGE,
     }) ,
-    prisma.teacher.count() 
+    prisma.teacher.count({where:{
+      lessons:{
+       some:{classId:parseInt(queryParams.classId!)}
+      }
+     }}), 
  ])
 
   return (
@@ -131,10 +141,10 @@ const TeacherListPage = async ({
         <div className="flex flex-col md:flex-row items-center gap-4 w-full md:w-auto">
           <TableSearch />
           <div className="flex items-center gap-4 self-end">
-            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
+            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-yellow">
               <Image src="/filter.png" alt="" width={14} height={14} />
             </button>
-            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
+            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-yellow">
               <Image src="/sort.png" alt="" width={14} height={14} />
             </button>
             {role === "admin" && (
