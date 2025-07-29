450858f920928e9b18c517b015e776de75f53f17
Ankita Goyal <goyalankita0325@gmail.com>
2025-07-29 13:37:14 +0530

    done with the student count chart

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,6 +1,6 @@
 import Announcements from "@/components/Announcements";
 import AttendanceChart from "@/components/AttendanceChart";
-import CountChart from "@/components/CountChart";
+import CountChartContainer from "@/components/CountChartContainer";
 import EventCalender from "@/components/EventCalender";
 import FinanceChart from "@/components/FinanceChart";
 import UserCard from "@/components/UserCard";
@@ -21,7 +21,7 @@ const AdminPage = () => {
 
         {/* Count Chart  */}
         <div className="w-full lg:w-1/3 h-[450px]">
-             <CountChart/>
+             <CountChartContainer/>
         </div>
 
         {/* Attendance chart */}
--- src/components/EventCalender.tsx
+++ src/components/EventCalender.tsx
@@ -31,11 +31,20 @@ const events = [
   ];
 
 const EventCalender = () =>  {
-    const [value, onChange] = useState<Value>(new Date());
+    const [value, setValue] = useState<Value>(new Date());
+
+    const handleChange = (val: Value) => {
+      if (
+        (val instanceof Date && !isNaN(val.getTime())) ||
+        (Array.isArray(val) && val.every(v => v instanceof Date && !isNaN(v.getTime())))
+      ) {
+        setValue(val);
+      }
+    };
 
     return (
       <div className='bg-white p-4 rounded-md'>
-        <Calendar onChange={onChange} value={value} />
+        <Calendar onChange={handleChange} value={value} />
         <div className='flex items-center justify-between'>
             <h1 className='text-xl font-semibold my-4'>Event</h1>
             <Image src="/moreDark.png" alt='' width={20} height={20} />
