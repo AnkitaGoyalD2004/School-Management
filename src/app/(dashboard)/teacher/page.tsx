d5a1a4973826dbc7319f24960651a0edcf168ddb
Ankita Goyal <goyalankita0325@gmail.com>
2025-08-02 20:38:32 +0530

    update

--- src/app/(dashboard)/teacher/page.tsx
+++ src/app/(dashboard)/teacher/page.tsx
@@ -1,14 +1,15 @@
-import Announcements from "../../../components/Announcements";
-import BigCalendar from "../../../components/BigCalender";
-
-const TeacherPage = () => {
+import Announcements from "@/components/Announcements";
+import BigCalendarContainer from "@/components/BigCalendarContainer";
+import { auth } from "@clerk/nextjs/server";
+const TeacherPage = async () => {
+  const { userId } = await auth();
   return (
     <div className="flex-1 p-4 flex gap-4 flex-col xl:flex-row">
       {/* LEFT */}
       <div className="w-full xl:w-2/3">
         <div className="h-full bg-white p-4 rounded-md">
-          <h1 className="text-xl font-semibold">Schedule (4A)</h1>
-          <BigCalendar/>
+          <h1 className="text-xl font-semibold">Schedule</h1>
+          <BigCalendarContainer type="teacherId" id={userId!} />
         </div>
       </div>
       {/* RIGHT */}
@@ -16,7 +17,7 @@ const TeacherPage = () => {
         <Announcements />
       </div>
     </div>
-  );  
+  );
 };
 
 export default TeacherPage;
--- src/components/BigCalender.tsx
+++ src/components/BigCalender.tsx
@@ -4,11 +4,14 @@ import moment from "moment";
 import { useState } from "react";
 import { Calendar, momentLocalizer, View, Views } from "react-big-calendar";
 import "react-big-calendar/lib/css/react-big-calendar.css";
-import { calendarEvents } from "../lib/data";
 
 const localizer = momentLocalizer(moment);
 
- const BigCalendar = () => {
+const BigCalendar = ({
+  data,
+}: {
+  data: { title: string; start: Date; end: Date }[];
+}) => {
   const [view, setView] = useState<View>(Views.WORK_WEEK);
 
   const handleOnChangeView = (selectedView: View) => {
@@ -18,13 +21,13 @@ const localizer = momentLocalizer(moment);
   return (
     <Calendar
       localizer={localizer}
-      events={calendarEvents}
+      events={data}
       startAccessor="start"
       endAccessor="end"
       views={["work_week", "day"]}
       view={view}
       style={{ height: "98%" }}
-       onView={handleOnChangeView}
+      onView={handleOnChangeView}
       min={new Date(2025, 1, 0, 8, 0, 0)}
       max={new Date(2025, 1, 0, 17, 0, 0)}
     />
